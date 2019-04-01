bf9d6761a53ad05a51e938ac2695a5bb9cc75eea
Wojciech Przybyła <wojprz@st.amu.edu.pl>
2019-04-01 22:21:26 +0200

    added category table

---
+++ api/PixBlocks_Addition.Domain/Interfaces/IDBCategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PixBlocks_Addition.Domain.Entities;
+
+namespace PixBlocks_Addition.Domain.Interfaces
+{
+    public interface IDBCategoryRepository
+    {
+        Category Add(Category category);
+        Category Get(int id);
+        IEnumerable<Category> GetAll();
+    }
+}
