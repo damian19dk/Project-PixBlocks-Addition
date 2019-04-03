e2a2ebc26c240e1ad49e3ad6de9c7726151db83d
Piotrek <p.popowski01@gmail.com>
2019-04-03 21:40:12 +0200

    Add IdentityController

---
+++ api/PixBlocks_Addition.Api/Controllers/IdentityController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PixBlocks_Addition.Api.ResourceModels;
+using PixBlocks_Addition.Domain.Repositories;
+using PixBlocks_Addition.Infrastructure.DTOs;
+using PixBlocks_Addition.Infrastructure.Services;
+
+namespace PixBlocks_Addition.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class IdentityController : ControllerBase
+    {
+        private readonly IIdentityService _identityService;
+        private readonly ICancellationTokenService _cancellationService;
+
+        public IdentityController(IIdentityService identityService, ICancellationTokenService cancellationTokenService)
+        {
+            _identityService = identityService;
+            _cancellationService = cancellationTokenService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<JwtDto> Login(LoginModel login)
+        {
+            return await _identityService.Login(login.Username, login.Password);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("refresh")]
+        public async Task<JwtDto> RefreshToken(string token)
+        {
+            return await _identityService.RefreshAccessToken(token);
+        }
+
+        [HttpPost("revoke")]
+        public async Task RevokeToken(string token)
+        {
+            await _identityService.RevokeRefreshToken(token);
+        }
+
+        [HttpPost("cancel")]
+        public async Task CancelToken()
+        {
+            await _cancellationService.DeactivateCurrentAsync();
+        }
+    }
+}
