9180b1241478195405121b7ccfed21dd6c666c19
Piotrek <p.popowski01@gmail.com>
2019-04-03 21:17:16 +0200

    Move refresh tokens

--- api/PixBlocks_Addition.Domain/Repositories/IRefreshTokenRepository.cs
+++ api/PixBlocks_Addition.Domain/Repositories/IRefreshTokenRepository.cs
@@ -1,14 +1,16 @@
-﻿using PixBlocks_Addition.Infrastructure.Models;
+﻿using PixBlocks_Addition.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PixBlocks_Addition.Infrastructure.Repositories
+namespace PixBlocks_Addition.Domain.Repositories
 {
     public interface IRefreshTokenRepository
     {
         Task<RefreshToken> GetAsync(string token);
+        Task<RefreshToken> GetByUserIdAsync(Guid userId);
         Task AddAsync(RefreshToken token);
+        Task UpdateAsync();
     }
 }
---
+++ api/PixBlocks_Addition.Domain/Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PixBlocks_Addition.Domain.Contexts;
+using PixBlocks_Addition.Domain.Entities;
+using PixBlocks_Addition.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PixBlocks_Addition.Infrastructure.Repositories
+{
+    public class RefreshTokenRepository : IRefreshTokenRepository
+    {
+        private readonly RefreshTokenContext _context;
+        public RefreshTokenRepository(RefreshTokenContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(RefreshToken token)
+        {
+            await _context.AddAsync(token);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<RefreshToken> GetAsync(string token)
+            => await _context.RefreshTokens.SingleOrDefaultAsync(x => x.Token == token);
+
+        public async Task<RefreshToken> GetByUserIdAsync(Guid userId)
+            => await _context.RefreshTokens.Where(x => x.UserId == userId && x.Revoked == false).FirstOrDefaultAsync();
+
+        public async Task UpdateAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
