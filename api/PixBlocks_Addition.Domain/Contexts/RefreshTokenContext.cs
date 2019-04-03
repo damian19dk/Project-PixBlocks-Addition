5dda6b73cd3bffe252926d2d2b9740ee6263f96e
Piotrek <p.popowski01@gmail.com>
2019-04-03 21:36:41 +0200

    Add Contexts

---
+++ api/PixBlocks_Addition.Domain/Contexts/RefreshTokenContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using PixBlocks_Addition.Domain.Entities;
+using PixBlocks_Addition.Domain.Settings;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PixBlocks_Addition.Domain.Contexts
+{
+    public class RefreshTokenContext : DbContext
+    {
+        private readonly SqlSettings _settings;
+
+        public RefreshTokenContext(DbContextOptions<RefreshTokenContext> options, IOptions<SqlSettings> sqlSettings) : base(options)
+        {
+            _settings = sqlSettings.Value;
+        }
+
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_settings.ConnectionString,
+                options=> 
+                {
+                    options.MigrationsAssembly("PixBlocks_Addition.Api");
+                });
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var itemBuilder = modelBuilder.Entity<RefreshToken>();
+            itemBuilder.HasKey(x => x.Id);
+        }
+    }
+}
