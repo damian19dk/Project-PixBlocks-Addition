a0ebcc8899a54a87b1782dadec0c74fa28967617
Piotrek <p.popowski01@gmail.com>
2019-04-03 21:19:49 +0200

    Add sql settings

---
+++ api/PixBlocks_Addition.Domain/Settings/SqlSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PixBlocks_Addition.Domain.Settings
+{
+    public class SqlSettings
+    {
+        public string ConnectionString { get; set; }
+    }
+}
