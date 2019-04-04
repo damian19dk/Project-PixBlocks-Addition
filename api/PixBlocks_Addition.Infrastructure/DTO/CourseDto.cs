0801bdf90c5846cb2129c53e8777464c88b583cf
wojprz <wojprz@st.amu.edu.pl>
2019-04-04 11:59:59 +0200

    added dto_s

---
+++ api/PixBlocks_Addition.Infrastructure/DTO/CourseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PixBlocks_Addition.Infrastructure.DTO
+{
+    public class CourseDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Descryption { get; set; }
+        public string Picture { get; set; }
+    }
+}
---
+++ api/PixBlocks_Addition.Infrastructure/DTO/VideoDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PixBlocks_Addition.Infrastructure.DTO
+{
+    public class VideoDto
+    {
+        public Guid Id { get; set; }
+        public Guid CourseId { get; set; }
+        public string VideoSrc { get; set; }
+    }
+}
