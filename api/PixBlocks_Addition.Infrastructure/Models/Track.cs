071f48f7048bec6f35f58b6ea5efbf64e27c66c0
Piotrek <p.popowski01@gmail.com>
2019-05-03 19:01:36 +0200

    Add JW Player models

---
+++ api/PixBlocks_Addition.Infrastructure/Models/Track.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PixBlocks_Addition.Infrastructure.Models
+{
+    public class Track
+    {
+        public string Kind { get; set; }
+        public string File { get; set; }
+    }
+}
