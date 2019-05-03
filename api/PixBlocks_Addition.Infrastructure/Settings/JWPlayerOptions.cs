1bab835ffb80eca0041a091fb28e7c0ed37d1f97
Piotrek <p.popowski01@gmail.com>
2019-05-03 19:00:11 +0200

    Add JW Player settings

---
+++ api/PixBlocks_Addition.Infrastructure/Settings/JWPlayerOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PixBlocks_Addition.Infrastructure.Settings
+{
+    public class JWPlayerOptions
+    {
+        public string Playlist { get; set; }
+    }
+}
