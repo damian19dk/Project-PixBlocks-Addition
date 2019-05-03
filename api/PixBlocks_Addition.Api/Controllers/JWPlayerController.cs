64bde6f1632f27dfd7fac9f48b6754c3ebacc6b7
Piotrek <p.popowski01@gmail.com>
2019-05-03 19:11:45 +0200

    Add JWPlayerController

---
+++ api/PixBlocks_Addition.Api/Controllers/JWPlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PixBlocks_Addition.Infrastructure.Models;
+using PixBlocks_Addition.Infrastructure.Services;
+
+namespace PixBlocks_Addition.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JWPlayerController : ControllerBase
+    {
+        private readonly IJWPlayerService _jwPlayer;
+
+        public JWPlayerController(IJWPlayerService jwPlayer)
+        {
+            _jwPlayer = jwPlayer;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("playlist")]
+        public async Task<Playlist> GetPlaylist(string id)
+        {
+            var token = getAuthorizationToken();
+            return await _jwPlayer.GetPlaylistAsync(id, token);
+        }
+
+        [AllowAnonymous]
+        [HttpGet("default")]
+        public async Task<Playlist> Get()
+        {
+            var token = getAuthorizationToken();
+            return await _jwPlayer.GetDefaultAsync(token);
+        }
+
+        private string getAuthorizationToken()
+        {
+            var token = HttpContext.Request.Headers["Authorization"].ToString();
+            token = token.Split(' ').Last();
+            return token;
+        }
+    }
+}
