dc5b513d70cb0aec1491d79367b75ca1b503203f
Piotrek <p.popowski01@gmail.com>
2019-05-12 12:34:29 +0200

    Add JwtPlayerHandler

---
+++ api/PixBlocks_Addition.Infrastructure/Services/IJwtPlayerHandler.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.IdentityModel.JsonWebTokens;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PixBlocks_Addition.Infrastructure.Services
+{
+    public interface IJwtPlayerHandler: IJwtHandler
+    {
+    }
+}
