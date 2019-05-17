f83c0aa5ac7dcd2b7819613b1e9e94aac39068a6
Wojciech Przybyła <wojprz@st.amu.edu.pl>
2019-05-17 16:12:39 +0200

    Create myexception.

---
+++ api/PixBlocks_Addition.Domain/Exceptions/MyException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PixBlocks_Addition.Domain.Exceptions
+{
+    public class MyException : Exception
+    {
+        private string code;
+        private string message1;
+
+        public MyException() { }
+
+        public MyException(string message) :base(message)
+        {
+
+        }
+
+        public MyException(string code, string message) :this(null, code, message)
+        {
+
+        }
+
+        public MyException(string message, string code, string message1) : this(message)
+        {
+            this.code = code;
+            this.message1 = message1;
+        }
+    }
+}
