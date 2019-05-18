1768958f1671da79f68cf5c5c6082a369570bf91
damian19dk <damian.kaczmarczyk@vp.pl>
2019-05-18 17:17:58 +0200

    Video Show Component Added

--- web/src/app/show-video/show-video.component.ts
+++ web/src/app/show-video/show-video.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-declare var jwplayer: any;
+import { Component, OnInit, Pipe, PipeTransform } from '@angular/core';
+import { Video } from '../models/video.model';
+import { VideoService } from '../services/video.service';
 
 @Component({
   selector: 'app-show-video',
@@ -8,9 +9,34 @@ declare var jwplayer: any;
 })
 export class ShowVideoComponent implements OnInit {
 
-  constructor() { }
+  private video: Video;
+
+  constructor(private videoService: VideoService) { }
 
   ngOnInit() {
+    this.video = new Video();
+    this.getVideo();
+  }
+
+  getVideo() {
+    this.videoService.getVideo("oWwRzyGf").subscribe(
+      (data: Video) => this.video = {
+        mediaId: data.mediaId,
+        description: data.description,
+        pubDate: data.pubDate,
+        tags: data.tags,
+        image: data.image,
+        title: data.title,
+        feedId: data.feedId,
+        sources: data.sources,
+        tracks: data.tracks,
+        link: data.link,
+        duration: data.duration
+      },
+      error => {
+        console.log("Błąd ładowania danych!");
+       });
+
 
   }
 
