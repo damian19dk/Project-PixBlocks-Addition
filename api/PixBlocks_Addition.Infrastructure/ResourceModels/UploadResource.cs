3584a8d508f4b286721ae36edd7c226c4019045f
Piotrek <p.popowski01@gmail.com>
2019-05-19 12:36:25 +0200

    Add resource models

--- api/PixBlocks_Addition.Infrastructure/ResourceModels/UploadResource.cs
+++ api/PixBlocks_Addition.Infrastructure/ResourceModels/UploadResource.cs
@@ -5,9 +5,9 @@ using System.Threading.Tasks;
 
 namespace PixBlocks_Addition.Infrastructure.ResourceModels
 {
-    public class LoginModel
+    public class UploadResource
     {
-        public string Login { get; set; }
-        public string Password { get; set; }
+        public string ParentName { get; set; }
+        public string MediaId { get; set; }
     }
 }
