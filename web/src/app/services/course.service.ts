2f820a511151ead73c80c84380e6f3fa92040a3b
damian19dk <damian.kaczmarczyk@vp.pl>
2019-05-20 00:21:20 +0200

    Course Service Modified

--- web/src/app/services/course.service.ts
+++ web/src/app/services/course.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Course } from '../models/course.model';
+import { HttpClient } from '@angular/common/http';
+import { LoadingService } from './loading.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
 
-  constructor() { }
+  private origin: string;
+
+  constructor(
+    private http: HttpClient,
+    private loadingService: LoadingService) {
+      this.origin = environment.baseUrl;
+     }
 
   getCourses() {
-    
+    return this.http.get<any>(this.origin + "/api/Course");
   }
 }
