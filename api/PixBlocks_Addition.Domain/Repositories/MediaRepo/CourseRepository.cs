7edae7ee42a7c6260f2d095d19c9a11ed78f7780
Piotrek <p.popowski01@gmail.com>
2019-05-19 12:12:25 +0200

    Move media repositories to MediaRepo

---
+++ api/PixBlocks_Addition.Domain/Repositories/MediaRepo/CourseRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PixBlocks_Addition.Domain.Contexts;
+using PixBlocks_Addition.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixBlocks_Addition.Domain.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly PixBlocksContext _entities;
+        public CourseRepository(PixBlocksContext entities)
+        {
+            _entities = entities;
+        }
+        public async Task<Course> GetAsync(Guid id) => await _entities.Courses.SingleOrDefaultAsync(x => x.Id == id);
+
+        public async Task<Course> GetAsync(string name) => await _entities.Courses.SingleOrDefaultAsync(x => x.Name == name);
+
+        public async Task<IEnumerable<Course>> GetAllAsync() => await _entities.Courses.ToListAsync();
+
+        public async Task AddAsync(Course course)
+        {
+            await _entities.Courses.AddAsync(course);
+            await _entities.SaveChangesAsync();
+        }
+
+        public async Task RemoveAsync(string name)
+        {
+            var course = await GetAsync(name);
+            _entities.Courses.Remove(course);
+            await _entities.SaveChangesAsync();
+        }
+
+        public async Task RemoveAsync(Guid id)
+        {
+            var course = await GetAsync(id);
+            _entities.Courses.Remove(course);
+            await _entities.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Course course)
+        {
+            _entities.Courses.Update(course);
+            await _entities.SaveChangesAsync();
+        }
+    }
+}
---
+++ api/PixBlocks_Addition.Domain/Repositories/MediaRepo/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using PixBlocks_Addition.Domain.Entities;
+
+namespace PixBlocks_Addition.Domain.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task AddAsync(Category category);
+        Task<Category> GetAsync(int id);
+        Task<Category> GetAsync(string name);
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task RemoveAsync(int id);
+        Task RemoveAsync(string name);
+    }
+}
