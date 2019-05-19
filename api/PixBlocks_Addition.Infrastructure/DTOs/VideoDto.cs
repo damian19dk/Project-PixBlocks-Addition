30163b6650df2ae770f34acb69508c34a663c479
Piotrek <p.popowski01@gmail.com>
2019-05-19 12:25:21 +0200

    Fix namespaces

--- api/PixBlocks_Addition.Infrastructure/DTOs/VideoDto.cs
+++ api/PixBlocks_Addition.Infrastructure/DTOs/VideoDto.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace PixBlocks_Addition.Infrastructure.DTO
+namespace PixBlocks_Addition.Infrastructure.DTOs
 {
     public class VideoDto
     {
