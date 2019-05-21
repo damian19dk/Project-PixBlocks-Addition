08294403d626afce78e657081faaf5eb5b1bf024
s430684 <damkac1@st.amu.edu.pl>
2019-05-21 18:33:58 +0200

    Some fixes

--- web/src/app/course-editor/course-editor.component.ts
+++ web/src/app/course-editor/course-editor.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CourseService } from '../services/course.service';
 import { Course } from '../models/course.model';
+import { LoadingService } from '../services/loading.service';
 
 @Component({
   selector: 'app-course-editor',
@@ -13,22 +14,26 @@ export class CourseEditorComponent implements OnInit {
   error: string;
 
   constructor(
-    private courseService: CourseService) { }
+    private courseService: CourseService,
+    private loadingService: LoadingService) { }
 
   ngOnInit() {
     this.getCourse();
   }
 
   getCourse() {
+    this.loadingService.load();
     this.courseService.getCourse("Sto twarzy grzybiarzy")
-    .subscribe(
-      (data: Course) => {
-        this.course = data;
-      },
-      error => {
-        this.error = error;
-      }
-    );
+      .subscribe(
+        (data: Course) => {
+          this.course = data;
+          this.loadingService.unload();
+        },
+        error => {
+          this.error = error;
+          this.loadingService.unload();
+        }
+      );
   }
 
 }
