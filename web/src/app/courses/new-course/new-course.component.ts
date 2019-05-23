ffde6f1f0abdeda214e9525b00cc9de785241031
damian19dk <damian.kaczmarczyk@vp.pl>
2019-05-23 23:33:07 +0200

    NewCourse View Added

--- web/src/app/courses/new-course/new-course.component.ts
+++ web/src/app/courses/new-course/new-course.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CourseService } from 'src/app/services/course.service';
-import { LoadingService } from 'src/app/services/loading.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { CourseDto } from 'src/app/models/courseDto.model';
 
 @Component({
   selector: 'app-new-course',
@@ -9,10 +11,57 @@ import { LoadingService } from 'src/app/services/loading.service';
 })
 export class NewCourseComponent implements OnInit {
 
-  constructor(private courseService: CourseService,
-    private loadingService: LoadingService) { }
+  newCourseForm: FormGroup;
+  loading: boolean;
+  submitted: boolean;
+  returnUrl: string;
+  courseDto: CourseDto;
+  error: string;
+
+  constructor(private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private courseService: CourseService) { }
 
   ngOnInit() {
+    this.courseDto = new CourseDto();
+
+    this.submitted = false;
+    this.loading = false;
+
+    this.newCourseForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
+
+  get f() { return this.newCourseForm.controls; }
+
+  createNewCourse() {
+    this.submitted = true;
+
+    if (this.newCourseForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+
+    this.courseDto = this.newCourseForm.value;
+    console.log(this.courseDto);
+    this.loading = false;
+
+    // this.courseService.addCourse(this.courseDto)
+    //   .subscribe(
+    //     data => {
+    //       this.loading = false;
+    //     },
+    //     error => {
+    //       this.error = error;
+    //       this.loading = false;
+    //     });
+  }
+
+  
 
 }
