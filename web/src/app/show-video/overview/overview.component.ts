ab2aa843c694d9bc95dfb98a633344fcd9248cb7
damian19dk <damian.kaczmarczyk@vp.pl>
2019-05-26 23:05:19 +0200

    BIG FIX

--- web/src/app/show-video/overview/overview.component.ts
+++ web/src/app/show-video/overview/overview.component.ts
@@ -1,8 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Video } from 'src/app/models/video.model';
-import { VideoService } from 'src/app/services/video.service';
-import { ActivatedRoute } from '@angular/router';
-import { LoadingService } from 'src/app/services/loading.service';
 
 @Component({
   selector: 'app-overview',
@@ -10,32 +7,12 @@ import { LoadingService } from 'src/app/services/loading.service';
   styleUrls: ['./overview.component.css']
 })
 export class OverviewComponent implements OnInit {
-  video: Video;
+  @Input() video: Video;
   error: string;
 
-  constructor(
-    private route: ActivatedRoute,
-    private videoService: VideoService,
-    private loadingService: LoadingService,
-  ) { }
+  constructor() { }
 
   ngOnInit() {
-    this.getVideo();
-  }
-  getVideo() {
-    this.loadingService.load();
-
-    const id = this.route.snapshot.paramMap.get('id');
-    this.videoService.getVideo(id).subscribe(
-      (data: Video) => {
-        this.video = data;
-        this.loadingService.unload();
-      },
-      error => {
-        this.error = error;
-        this.loadingService.unload();
-      });
-
   }
 
 }
