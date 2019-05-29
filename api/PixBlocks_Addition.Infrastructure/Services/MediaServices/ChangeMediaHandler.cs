feebbd2ac7cc594a5cba7f76db0b336d59eff1a2
Piotrek <p.popowski01@gmail.com>
2019-05-29 14:12:09 +0200

    Add ChangeMediaHandler

---
+++ api/PixBlocks_Addition.Infrastructure/Services/MediaServices/ChangeMediaHandler.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PixBlocks_Addition.Domain.Entities;
+using PixBlocks_Addition.Domain.Exceptions;
+using PixBlocks_Addition.Domain.Repositories;
+using PixBlocks_Addition.Domain.Repositories.MediaRepo;
+using PixBlocks_Addition.Infrastructure.ResourceModels;
+
+namespace PixBlocks_Addition.Infrastructure.Services.MediaServices
+{
+    public class ChangeMediaHandler<TEntity>: IChangeMediaHandler<TEntity> where TEntity : Media
+    {
+        private readonly IImageHandler _imageHandler;
+        private readonly IImageRepository _imageRepository;
+
+        public ChangeMediaHandler(IImageHandler imageHandler, IImageRepository imageRepository)
+        {
+            _imageHandler = imageHandler;
+            _imageRepository = imageRepository;
+        }
+
+        public async Task ChangeAsync(ChangeMediaResource resource, IMediaRepository<TEntity> mediaRepository, IMediaRepository<TEntity> parentRepository = null)
+        {
+            var entity = await mediaRepository.GetAsync(resource.Id);
+            if (entity == null)
+            {
+                throw new MyException($"Media with id {resource.Id} not found. Create the media first.");
+            }
+            if (entity.Title != resource.Title)
+            {
+                IEnumerable<TEntity> sameTitle;
+                if (parentRepository == null)
+                    sameTitle = await mediaRepository.GetAsync(resource.Title);
+                else
+                    sameTitle = await parentRepository.GetAsync(resource.Title);
+
+                if (sameTitle.Count() > 0)
+                {
+                    throw new MyException($"There is already a media with title {resource.Title}.");
+                }
+                entity.SetTitle(resource.Title);
+            }
+            if (resource.Description != entity.Description)
+                entity.SetDescription(resource.Description);
+            if (resource.Language != entity.Language)
+                entity.SetLanguage(resource.Language);
+            if (resource.Premium != entity.Premium)
+                entity.SetPremium(resource.Premium);
+
+            resource.Tags = resource.Tags.First().Replace("\"", string.Empty).Replace("\\", string.Empty).Split(',', ' ');
+            //Remove tags
+            ISet<Tag> tagsToRemove = new HashSet<Tag>();
+            foreach (var tag in entity.Tags)
+            {
+                if (!resource.Tags.Contains(tag.Name))
+                {
+                    tagsToRemove.Add(tag);
+                }
+            }
+            await mediaRepository.RemoveTagsAsync(entity, tagsToRemove);
+            //Add tags
+            foreach (var tag in resource.Tags)
+            {
+                if (!entity.Tags.Any(t => t.Name == tag))
+                {
+                    entity.Tags.Add(new Tag(tag));
+                }
+            }
+
+            //Add picture from url
+            if (resource.PictureUrl != null)
+            {
+                await tryRemoveImageFromDb(entity.Picture);
+                entity.SetPicture(resource.PictureUrl);
+            }
+            //Add picture to database
+            if (resource.Image != null)
+            {
+                await tryRemoveImageFromDb(entity.Picture);
+                var img = await _imageHandler.CreateAsync(resource.Image);
+                await _imageRepository.AddAsync(img);
+                entity.SetPicture(img.Id.ToString());
+            }
+
+            await mediaRepository.UpdateAsync(entity);
+        }
+
+        private async Task tryRemoveImageFromDb(string coursePicture)
+        {
+            //Remove picture from database
+            if (!string.IsNullOrWhiteSpace(coursePicture))
+            {
+                Guid id;
+                if (Guid.TryParse(coursePicture, out id))
+                {
+                    var imageFromDb = await _imageRepository.GetAsync(id);
+                    if (imageFromDb != null)
+                        await _imageRepository.RemoveAsync(id);
+                }
+            }
+        }
+    }
+}
---
+++ api/PixBlocks_Addition.Infrastructure/Services/MediaServices/IChangeMediaHandler.cs
@@ -0,0 +1,15 @@
+﻿using PixBlocks_Addition.Domain.Entities;
+using PixBlocks_Addition.Domain.Repositories.MediaRepo;
+using PixBlocks_Addition.Infrastructure.ResourceModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixBlocks_Addition.Infrastructure.Services.MediaServices
+{
+    public interface IChangeMediaHandler<TEntity> where TEntity: Media
+    {
+        Task ChangeAsync(ChangeMediaResource resource, IMediaRepository<TEntity> mediaRepository, IMediaRepository<TEntity> parentRepository = null);
+    }
+}
