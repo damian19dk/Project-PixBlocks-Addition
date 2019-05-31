e5b2473a0ec614d34a8ccccaeeb2e7d55cbf3aae
damian19dk <damian.kaczmarczyk@vp.pl>
2019-05-31 23:34:53 +0200

    JwtInterceptor Added

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -35,6 +35,7 @@ import { NewLessonComponent } from './lessons/new-lesson/new-lesson.component';
 import { EditLessonComponent } from './lessons/edit-lesson/edit-lesson.component';
 import { TokenInterceptor } from './interceptors/token-interceptor';
 import { AccessControlAllowOriginInterceptor } from './interceptors/access-control-allow-origin-interceptor';
+import { JwtInterceptor } from './interceptors/jwt-interceptor';
 
 
 const routes: Routes = [
@@ -105,6 +106,11 @@ const routes: Routes = [
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AccessControlAllowOriginInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
       multi: true
     }
   ],
---
+++ web/src/app/interceptors/jwt-interceptor.ts
@@ -0,0 +1,28 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { AuthenticationService } from '../services/authentication.service';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+
+    returnUrl: string;
+
+    constructor(public route: ActivatedRoute,
+        public router: Router,
+        public authenticationService: AuthenticationService) {
+            this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || 'unauthorized';
+         }
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+        return next.handle(request).pipe(catchError(err => {
+            if (err.status === 401) {
+                this.router.navigate([this.returnUrl]);
+            }
+            const error = err.error.message || err.statusText;
+            return throwError(error);
+        }));
+    }
+}
