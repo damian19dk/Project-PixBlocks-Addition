7df58204bb47684c6b3f9ee96b263855fc0f10df
Piotrek <p.popowski01@gmail.com>
2019-06-03 18:02:56 +0200

    Extend create signature method

--- api/PixBlocks_Addition.Infrastructure/Services/IJWPlayerAuthHandler.cs
+++ api/PixBlocks_Addition.Infrastructure/Services/IJWPlayerAuthHandler.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace PixBlocks_Addition.Infrastructure.Services
 {
     public interface IJWPlayerAuthHandler
     {
-        string CreateSignature(string api_key, string secret, string format = "json");
+        string CreateSignature(string api_key, string secret, string format = "json", IDictionary<string, string> parameters = null);
     }
 }
