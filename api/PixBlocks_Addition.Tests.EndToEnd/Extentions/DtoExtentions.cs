59dfe9bb6c18a6539a101715d993f7610de69475
Piotrek <p.popowski01@gmail.com>
2019-06-07 20:39:31 +0200

    Add getting properties of MediaDto extention method

---
+++ api/PixBlocks_Addition.Tests.EndToEnd/Extentions/DtoExtentions.cs
@@ -0,0 +1,43 @@
+﻿using PixBlocks_Addition.Infrastructure.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PixBlocks_Addition.Tests.EndToEnd.Extentions
+{
+    public static class DtoExtentions
+    {
+        private static string[] arrayProperties = { "Tags" };
+        private static string[] useToStringForProperties = { "Id", "Premium", "PublishDate", "Duration" };
+
+        public static Dictionary<string, string> GetProperties<MediaDto, TResult>(this MediaDto lesson, Func<MediaDto, TResult> func)
+        {
+            var parameters = func(lesson);
+            var properties = parameters.GetType().GetProperties();
+            var dict = new Dictionary<string, string>();
+
+            foreach (var p in properties)
+            {
+                if (arrayProperties.Contains(p.Name))
+                {
+                    dict.Add(p.Name, string.Join(',', (IEnumerable<string>)p.GetValue(parameters)));
+                }
+                else if(useToStringForProperties.Contains(p.Name))
+                {
+                    dict.Add(p.Name, p.GetValue(parameters).ToString());
+                }
+                else if(p.Name=="Picture")
+                {
+                    dict.Add("PictureUrl", (string)p.GetValue(parameters));
+                }
+                else
+                {
+                    dict.Add(p.Name, (string)p.GetValue(parameters));
+                }
+            }
+
+            return dict;
+        }
+    }
+}
