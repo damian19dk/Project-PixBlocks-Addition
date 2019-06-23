b0eac9b16147dcb460839be637afbfd62c26fe81
Wojciech Przybyła <wojprz@st.amu.edu.pl>
2019-06-23 20:42:43 +0200

    2 tests

--- api/PixBlocks_Addition.Tests.EndToEnd/TestControllers/LessonControllerTest.cs
+++ api/PixBlocks_Addition.Tests.EndToEnd/TestControllers/LessonControllerTest.cs
@@ -70,5 +70,81 @@ namespace PixBlocks_Addition.Tests.EndToEnd.TestControllers
 
             Assert.IsTrue(JsonConvert.SerializeObject(lesson) == JsonConvert.SerializeObject(expectedLesson));
         }
+
+        [Test]
+        public async Task get_from_wrong_name_should_return_empty()
+        {
+            var wrongTitle = "Cos innego";
+            var response = await httpClient.GetAsync($"api/lesson/title?/title={wrongTitle}");
+            var responseString = await response.Content.ReadAsStringAsync();
+
+
+            Assert.IsTrue(responseString.Contains("[]"));
+        }
+
+        [Test]
+        public async Task get_from_good_name_should_return_lesson()
+        {
+            var goodtitle = "LessonTitle1";
+
+            var expectedTitle = "LessonTitle1";
+            var expectedPremium = false;
+            var expectedLanguage = "English";
+            var expectedDescription = "Description lesson.";
+            var expectedTags = "Granie na fortepianie,inne";
+
+            var parameters = new Dictionary<string, string>();
+            parameters.Add("Title", "LessonTitle1");
+            parameters.Add("Description", "Description lesson.");
+            parameters.Add("Premium", "false");
+            parameters.Add("Language", "English");
+            parameters.Add("ParentId", CourseDto.Id.ToString());
+
+            var expectedLesson = new LessonDto()
+            {
+                Title = expectedTitle,
+                Description = expectedDescription,
+                Language = expectedLanguage,
+                Premium = expectedPremium,
+                Id = LessonDto.Id,
+                MediaId = LessonDto.MediaId,
+                Duration = LessonDto.Duration,
+                LessonVideos = LessonDto.LessonVideos,
+                Exercises = LessonDto.Exercises,
+                Picture = LessonDto.Picture,
+                //PublishDate = LessonDto.PublishDate,
+                Tags = expectedTags.Split(',')
+            };
+            
+
+
+            sendMultiPartAsync("api/lesson/create", "POST", parameters).Wait();
+            var response = await httpClient.GetAsync($"api/lesson?title={expectedTitle}");
+            var responseString = await response.Content.ReadAsStringAsync();
+            var lesson = JsonConvert.DeserializeObject<LessonDto>(responseString);
+           
+            Assert.IsTrue(JsonConvert.SerializeObject(lesson).ToLower() == JsonConvert.SerializeObject(expectedLesson).ToLower());
+        }
+
+        private bool checkLessonData(IDictionary<string, string> data, LessonDto lesson)
+        {
+            foreach (var p in data)
+            {
+                if (p.Key.ToLowerInvariant() != "tags")
+                {
+                    if (p.Value != lesson.GetType().GetProperty(p.Key).GetValue(lesson).ToString())
+                        return false;
+                }
+                else
+                {
+                    foreach (var tag in lesson.Tags)
+                    {
+                        if (!data["Tags"].Contains(tag))
+                            return false;
+                    }
+                }
+            }
+            return true;
+        }
     }
 }
---
+++ api/PixBlocks_Addition.Tests.EndToEnd/TestControllers/UserControllerTestcs.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using PixBlocks_Addition.Api;
+using PixBlocks_Addition.Infrastructure.DTOs;
+using PixBlocks_Addition.Infrastructure.ResourceModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+namespace PixBlocks_Addition.Tests.EndToEnd.TestControllers
+{
+    [TestFixture]
+    public abstract class UserControllerTest : BaseControllerTest
+    {
+        public UserDto UserDto { get; set; }
+
+        public UserControllerTest()
+        {
+            Init().Wait();
+        }
+
+        private async Task Init()
+        {
+            var parameters = new Dictionary<string, string>();
+            parameters.Add("Login", "Login1");
+            parameters.Add("E_mail", "email1@wp.pl");
+            parameters.Add("Password", "password1");
+            parameters.Add("RoleId", "2");
+            parameters.Add("Is_premium", "true");
+            parameters.Add("Status", "1");
+
+            sendMultiPartAsync("api/identity/register", "POST", parameters).Wait();
+
+            var response = await httpClient.GetAsync("api/user/login?login=Login1");
+            var responseString = await response.Content.ReadAsStringAsync();
+            var user = JsonConvert.DeserializeObject<IEnumerable<UserDto>>(responseString);
+            UserDto = user.Single();
+        }
+
+        [Test]
+        public async Task changing_password_should_work()
+        {
+
+            var parameters = new Dictionary<string, string>();
+            parameters.Add("newPassword", "password2");
+            parameters.Add("login", "Login1");
+            parameters.Add("oldPassword", "password1");
+
+            var Login = "Login1";
+            var oldPassword = "password2";
+
+            var parameters2 = new Dictionary<string, string>();
+            parameters2.Add("Login", Login);
+            parameters2.Add("Password", oldPassword);
+
+            await sendMultiPartAsync("api/user/passwordChange", "PUT", parameters);
+            Assert.Fail("api/identity/login", "POST", parameters2);
+        }
+    }
+}
