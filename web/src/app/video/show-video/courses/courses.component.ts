4afa9de43226e8967af54029b8f8465662d86b9a
Jedrek <jedrek_nicinski@op.pl>
2019-11-16 17:32:04 +0100

    sth new

--- web/src/app/video/show-video/courses/courses.component.ts
+++ web/src/app/video/show-video/courses/courses.component.ts
@@ -1,94 +1,4 @@
-/*import { Component, OnInit, Input } from '@angular/core';
-import { HostedCourseDocument } from '../../../models/hostedCourseDocument.model';
-import { CourseService } from '../../../services/course.service';
-import { ActivatedRoute } from '@angular/router';
-import { LoadingService } from '../../../services/loading.service';
-import { CourseDocument } from 'src/app/models/courseDocument.model';
 
-
-@Component({
-  selector: 'app-courses',
-  templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.css']
-})
-export class CoursesComponent implements OnInit {
-  @Input() course: CourseDocument = null;
-  @Output() editCourseComponent: EventEmitter<any> = new EventEmitter<any>();
-  image: any;
-  error: string;
- // mediaId: string;
-  editCourseForm: FormGroup;
-  loading: boolean;
-  submitted: boolean;
-  sent: boolean;
-  returnUrl: string;
-  courseDto: CourseDto;
-  error: string;
-  tagsList = [];
-  tagsSettings = {};
-
-  fileToUpload: File = null;
-  fileUploadMessage: string = 'Wybierz plik';
-
-  constructor(
-    private route: ActivatedRoute,
-    private courseService: CourseService,
-    private loadingService: LoadingService,
-    private modalService: NgbModal,
-    private formBuilder: FormBuilder,
-  ) { }
-
-  ngOnInit() {
-    this.getCourses();
-    this.getPicture();
-
-    this.tagsList = this.tagService.getTags();
-    this.tagsSettings = this.tagService.getTagSettingsForMultiselect();
-
-    this.courseDto = new CourseDto();
-
-    this.sent = false;
-    this.submitted = false;
-    this.loading = false;
-    this.error = null;
-
-    this.editCourseForm = this.formBuilder.group({
-      parentId: [null],
-      id: [this.course.id],
-      title: [this.course.title, Validators.required],
-      description: [this.course.description, [Validators.required, Validators.minLength(3), Validators.maxLength(10000)]],
-      premium: [this.course.premium],
-      tags: [this.tagService.toTagsList(this.course.tags)],
-      language: [this.course.language],
-      pictureUrl: [this.course.picture],
-      image: [null]
-    });
-  }
-
-  getCourses() {
-    this.loadingService.load();
-    const mediaId = this.route.snapshot.paramMap.get('id');
-    this.courseService.getCourse(mediaId).subscribe(
-      (data: CourseDocument) => {
-        this.courseDocument = data;
-        this.loadingService.unload();
-        },
-      error => {
-        this.error = error;
-        this.loadingService.unload();
-      }
-    );
-  }
-  private getPicture() {
-    let picture = null;
-    if (this.course.picture == null) {
-      this.course.picture = "https://mdrao.ca/wp-content/uploads/2018/03/DistanceEdCourse_ResitExam.png";
-      return;
-    }
-  }
-
-}
-*/
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
@@ -97,7 +7,7 @@ import { CourseDto } from 'src/app/models/courseDto.model';
 import { CourseService } from 'src/app/services/course.service';
 import { LoadingService } from '../../../services/loading.service';
 import { ActivatedRoute } from '@angular/router';
-// import { HostedCourseDocument } from '../../models/hostedCourseDocument.model';
+
 
 @Component({
   selector: 'app-courses',
