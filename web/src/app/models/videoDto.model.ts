e3c36ba91b1797f394c303468af4d3e89505ffa6
dkaczmarczyk <dkaczmarczyk@itti.com.pl>
2019-11-24 19:17:17 +0100

    Fix na dodawanie video

--- web/src/app/models/videoDto.model.ts
+++ web/src/app/models/videoDto.model.ts
@@ -1,6 +1,12 @@
-import { DataDto } from './dataDto';
+import {DataDto} from './dataDto';
 
 export class VideoDto extends DataDto {
     mediaId: string;
     video: any;
+
+  toFormData(): FormData {
+    const formData = super.toFormData();
+    this.video !== null ? formData.append('video', this.video) : null;
+    return formData;
+  }
 }
