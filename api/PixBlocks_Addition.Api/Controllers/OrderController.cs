0e733ba0ce03a8869688bbbf32af4110a07561d1
Piotrek <p.popowski01@gmail.com>
2019-12-13 16:14:22 +0100

    Add OrderController

---
+++ api/PixBlocks_Addition.Api/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PixBlocks_Addition.Infrastructure.Services;
+
+namespace PixBlocks_Addition.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpPost("courses")]
+        public async Task OrderCourses(IEnumerable<Guid> courses)
+        {
+            await _orderService.OrderCourses(courses);
+        }
+
+        [HttpPost("videos")]
+        public async Task OrderVideos(IEnumerable<Guid> videos, Guid courseId)
+        {
+            await _orderService.OrderVideos(videos, courseId);
+        }
+    }
+}
