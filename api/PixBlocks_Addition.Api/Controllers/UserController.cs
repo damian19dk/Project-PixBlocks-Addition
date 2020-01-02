c5e821609f601185b9851b4f8e113d90ef0c5168
Wojciech Przybyła <wojprz@st.amu.edu.pl>
2020-01-02 14:53:59 +0100

    deliting history methods from usercontroller

--- api/PixBlocks_Addition.Api/Controllers/UserController.cs
+++ api/PixBlocks_Addition.Api/Controllers/UserController.cs
@@ -50,41 +50,5 @@ namespace PixBlocks_Addition.Api.Controllers
         {
             await _userService.TakePremium(login);
         }
-
-        [HttpDelete("deleteUserHistoryId")]
-        public async Task RemoveUserhistory(Guid userId)
-        {
-            await _userCourseHistoryService.RemoveAsync(userId);
-        }
-
-        [HttpDelete("deleteUserHistory")]
-        public async Task RemoveUserhistory(string login)
-        {
-            await _userCourseHistoryService.RemoveAsync(login);
-        }
-
-        [HttpPost("addCourseToHistoryId")]
-        public async Task AddCourseToHistory(Guid userId, Guid courseId)
-        {
-            await _userCourseHistoryService.AddHistoryAsync(userId, courseId);
-        }
-
-        [HttpPost("addCourseToHistory")]
-        public async Task AddCourseToHistory(string login, Guid courseId)
-        {
-            await _userCourseHistoryService.AddHistoryAsync(login, courseId);
-        }
-
-        [HttpGet("getUserHistoryId")]
-        public async Task<IEnumerable<CourseDto>> GetUserHistory(Guid userId)
-        {
-            return await _userCourseHistoryService.GetAllAsync(userId);
-        }
-
-        [HttpGet("getUserHistory")]
-        public async Task<IEnumerable<CourseDto>> GetUserHistory(string login)
-        {
-            return await _userCourseHistoryService.GetAllAsync(login);
-        }
     }
 }
