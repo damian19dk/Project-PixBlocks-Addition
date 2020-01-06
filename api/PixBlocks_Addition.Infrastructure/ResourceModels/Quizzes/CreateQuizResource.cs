d8c0276f91603d5e5e6832e5a530e791bb750c0f
Piotrek <p.popowski01@gmail.com>
2020-01-06 16:17:05 +0100

    Add Quiz resources

---
+++ api/PixBlocks_Addition.Infrastructure/ResourceModels/Quizzes/CreateQuizResource.cs
@@ -0,0 +1,14 @@
+﻿using PixBlocks_Addition.Infrastructure.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PixBlocks_Addition.Infrastructure.ResourceModels.Quizzes
+{
+    public class CreateQuizResource
+    {
+        public Guid MediaId { get; set; }
+        public string Question { get; set; }
+        public ICollection<QuizAnswerResource> Answers { get; set; }
+    }
+}
---
+++ api/PixBlocks_Addition.Infrastructure/ResourceModels/Quizzes/UpdateQuizResource.cs
@@ -0,0 +1,14 @@
+﻿using PixBlocks_Addition.Infrastructure.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PixBlocks_Addition.Infrastructure.ResourceModels.Quizzes
+{
+    public class UpdateQuizResource
+    {
+        public Guid QuizId { get; set; }
+        public string Question { get; set; }
+        public ICollection<QuizAnswerResource> Answers { get; set; }
+    }
+}
