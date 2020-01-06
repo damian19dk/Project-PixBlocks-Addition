048f169ca69e61d856d8299c8ae19a6cbc6b575d
Piotrek <p.popowski01@gmail.com>
2020-01-06 16:10:53 +0100

    Add Quiz repository

---
+++ api/PixBlocks_Addition.Domain/Repositories/IQuizRepository.cs
@@ -0,0 +1,17 @@
+﻿using PixBlocks_Addition.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixBlocks_Addition.Domain.Repositories
+{
+    public interface IQuizRepository
+    {
+        Task AddAsync(Quiz quiz);
+        Task<Quiz> GetAsync(Guid id);
+        Task<Quiz> GetAsync(Media media);
+        Task UpdateAsync(Quiz quiz);
+        Task RemoveAsync(Quiz quiz);
+    }
+}
---
+++ api/PixBlocks_Addition.Domain/Repositories/QuizRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PixBlocks_Addition.Domain.Contexts;
+using PixBlocks_Addition.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixBlocks_Addition.Domain.Repositories
+{
+    public class QuizRepository: IQuizRepository
+    {
+        private readonly PixBlocksContext _entities;
+
+        public QuizRepository(PixBlocksContext context)
+        {
+            _entities = context;
+        }
+
+        public async Task<Quiz> GetAsync(Guid id) => await _entities.Quizzes.Include(q=>q.Answers).SingleOrDefaultAsync(x => x.Id == id);
+
+        public async Task<Quiz> GetAsync(Media media) => await _entities.Quizzes.Include(q => q.Answers).SingleOrDefaultAsync(x => x.MediaId == media.Id);
+
+        public async Task AddAsync(Quiz quiz)
+        {
+            _entities.Quizzes.Add(quiz);
+            await _entities.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Quiz quiz)
+        {
+            _entities.Quizzes.Update(quiz);
+            await _entities.SaveChangesAsync();
+        }
+
+        public async Task RemoveAsync(Quiz quiz)
+        {
+            _entities.Quizzes.Remove(quiz);
+            await _entities.SaveChangesAsync();
+        }
+        
+    }
+}
