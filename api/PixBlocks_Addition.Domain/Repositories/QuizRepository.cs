93d2af1e877716f3d9d83628d0710472c666c90f
Piotrek <p.popowski01@gmail.com>
2020-01-06 20:49:26 +0100

    Fix removing

--- api/PixBlocks_Addition.Domain/Repositories/QuizRepository.cs
+++ api/PixBlocks_Addition.Domain/Repositories/QuizRepository.cs
@@ -12,15 +12,16 @@ namespace PixBlocks_Addition.Domain.Repositories
     public class QuizRepository: IQuizRepository
     {
         private readonly PixBlocksContext _entities;
-
+        private readonly IQueryable<Quiz> _quizzes;
         public QuizRepository(PixBlocksContext context)
         {
             _entities = context;
+            _quizzes = context.Quizzes.Include(x => x.Questions).ThenInclude(x => x.Answers);
         }
 
-        public async Task<Quiz> GetAsync(Guid id) => await _entities.Quizzes.Include(q=>q.Answers).SingleOrDefaultAsync(x => x.Id == id);
+        public async Task<Quiz> GetAsync(Guid id) => await _quizzes.SingleOrDefaultAsync(x => x.Id == id);
 
-        public async Task<Quiz> GetAsync(Media media) => await _entities.Quizzes.Include(q => q.Answers).SingleOrDefaultAsync(x => x.MediaId == media.Id);
+        public async Task<Quiz> GetAsync(Media media) => await _quizzes.SingleOrDefaultAsync(x => x.MediaId == media.Id);
 
         public async Task<Media> GetMediaAsync(Guid quizId)
         {
@@ -40,16 +41,27 @@ namespace PixBlocks_Addition.Domain.Repositories
 
         public async Task UpdateAsync(Quiz quiz)
         {
-            var answers = await _entities.QuizAnswers.Where(x => x.QuizId == quiz.Id).ToListAsync();
-            _entities.QuizAnswers.RemoveRange(answers);
+            var questions = await _entities.QuizQuestions.Where(x => x.QuizId == quiz.Id).ToListAsync();
+            foreach(var question in questions)
+            {
+                var answers = await _entities.QuizAnswers.Where(x => x.QuizQuestionId == question.Id).ToListAsync();
+                _entities.QuizAnswers.RemoveRange(answers);
+            }
+            _entities.QuizQuestions.RemoveRange(questions);
+
             _entities.Quizzes.Update(quiz);
             await _entities.SaveChangesAsync();
         }
 
         public async Task RemoveAsync(Quiz quiz)
         {
-            var answers = await _entities.QuizAnswers.Where(x => x.QuizId == quiz.Id).ToListAsync();
-            _entities.QuizAnswers.RemoveRange(answers);
+            var questions = await _entities.QuizQuestions.Where(x => x.QuizId == quiz.Id).ToListAsync();
+            foreach (var question in questions)
+            {
+                var answers = await _entities.QuizAnswers.Where(x => x.QuizQuestionId == question.Id).ToListAsync();
+                _entities.QuizAnswers.RemoveRange(answers);
+            }
+            _entities.QuizQuestions.RemoveRange(questions);
             _entities.Quizzes.Remove(quiz);
             await _entities.SaveChangesAsync();
         }
