1af8b00376d640159ebe9014facdbe5d7fe509ea
Piotrek <p.popowski01@gmail.com>
2020-01-09 21:04:38 +0100

    Fix VideoHistory service

--- api/PixBlocks_Addition.Infrastructure/Services/IVideoHistoryService.cs
+++ api/PixBlocks_Addition.Infrastructure/Services/IVideoHistoryService.cs
@@ -8,9 +8,8 @@ namespace PixBlocks_Addition.Infrastructure.Services
 {
     public interface IVideoHistoryService
     {
-        Task AddAsync(Guid userId, Guid videoId, long time = 0);
-        Task UpdateAsync(Guid userId, Guid videoId, long time = 0);
-        Task<VideoHistoryDto> GetUserVideoHistory(Guid userId);
-        Task<int> GetUserProgres(Guid userId, Guid courseId);
+        Task SetProgressAsync(Guid userId, Guid videoId, long time = 0);
+        Task<VideoHistoryDto> GetHistoryAsync(Guid userId);
+        Task<int> GetUserProgressAsync(Guid userId, Guid courseId);
     }
 }
--- api/PixBlocks_Addition.Infrastructure/Services/VideoHistoryService.cs
+++ api/PixBlocks_Addition.Infrastructure/Services/VideoHistoryService.cs
@@ -7,6 +7,7 @@ using PixBlocks_Addition.Infrastructure.DTOs;
 using PixBlocks_Addition.Infrastructure.Mappers;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,67 +19,48 @@ namespace PixBlocks_Addition.Infrastructure.Services
         private readonly IVideoRepository _videoRepository;
         private readonly IMapper _mapper;
         private readonly ICourseRepository _courseRepository;
-        private readonly IVideoHistoryHelperRepository _videoHistoryHelperRepository;
+        private readonly IVideoHistoryRepository _videoHistoryRepository;
 
         public VideoHistoryService(IUserRepository userRepository, IVideoRepository videoRepository, 
             IAutoMapperConfig config, ICourseRepository courseRepository,
-            IVideoHistoryHelperRepository videoHistoryHelperRepository)
+            IVideoHistoryRepository videoHistoryRepository)
         {
             _userRepository = userRepository;
             _videoRepository = videoRepository;
             _mapper = config.Mapper;
             _courseRepository = courseRepository;
-            _videoHistoryHelperRepository = videoHistoryHelperRepository;
+            _videoHistoryRepository = videoHistoryRepository;
         }
 
-        public async Task AddAsync(Guid userId, Guid videoId, long time = 0)
+        public async Task SetProgressAsync(Guid userId, Guid videoId, long time = 0)
         {
             var user = await _userRepository.GetAsync(userId);
-            var video = await _videoRepository.GetAsync(videoId);
-            var videoHistory = await _videoHistoryRepository.GetAsync(user);
-            if (user == null) throw new MyException(MyCodesNumbers.UserNotFound, MyCodes.UserNotFound);
-            if (video == null) throw new MyException(MyCodesNumbers.VideoNotFound, MyCodes.VideoNotFound);
-            if (videoHistory == null) throw new MyException(MyCodesNumbers.VideoHistoryNotFound, MyCodes.VideoHistoryNotFound);
-            if (time < 0) throw new MyException(MyCodesNumbers.NegativeTime, MyCodes.NegativeTime);
+            if (user == null)
+                throw new MyException(MyCodesNumbers.UserNotFound, MyCodes.UserNotFound);
 
-            foreach (var i in videoHistory.Videos)
+            var video = await _videoRepository.GetAsync(videoId);
+            if (video == null)
+                throw new MyException(MyCodesNumbers.VideoNotFound, MyCodes.VideoNotFound);
+
+            var videoHistory = await _videoHistoryRepository.GetAsync(user);
+            if (videoHistory == null)
+                throw new MyException(MyCodesNumbers.VideoHistoryNotFound, MyCodes.VideoHistoryNotFound);
+
+            VideoRecord videoRecord = videoHistory.Videos.SingleOrDefault(v => v.Video.Id == videoId);
+            if(videoRecord == null)
             {
-                if (i.Video == video) throw new MyException(MyCodesNumbers.VideoInHistory, MyCodes.VideoInHistory);
+                videoRecord = new VideoRecord(video, time);
+                videoHistory.Videos.Add(videoRecord);
             }
-            var helper = new VideoRecord(video, time);
-
-            
-            await _videoHistoryHelperRepository.AddAsync(helper);
-            videoHistory.Videos.Add(helper);
-
-        }
-
-        public async Task UpdateAsync(Guid userId, Guid videoId, long time = 0)
-        {
-            var user = await _userRepository.GetAsync(userId);
-            var video = await _videoRepository.GetAsync(videoId);
-            var videoHistory = await _videoHistoryRepository.GetAsync(user);
-            if (user == null) throw new MyException(MyCodesNumbers.UserNotFound, MyCodes.UserNotFound);
-            if (video == null) throw new MyException(MyCodesNumbers.VideoNotFound, MyCodes.VideoNotFound);
-            if (videoHistory == null) throw new MyException(MyCodesNumbers.VideoHistoryNotFound, MyCodes.VideoHistoryNotFound);
-            if (time < 0) throw new MyException(MyCodesNumbers.NegativeTime, MyCodes.NegativeTime);
-
-            long helper = 0;
-            foreach(var i in videoHistory.Videos)
+            else
             {
-                if (i.Video == video)
-                    helper = i.Time;
+                videoRecord.SetTime(time);
             }
 
-            videoHistory.Videos.Remove(new VideoRecord(video, helper));
-
-            videoHistory.Videos.Add(new VideoRecord(video, time));
-
-            //await _videoHistoryHelperRepository.UpdateAsync(videoHistory.Videos);
             await _videoHistoryRepository.UpdateAsync(videoHistory);
         }
 
-        public async Task<VideoHistoryDto> GetUserVideoHistory(Guid userId)
+        public async Task<VideoHistoryDto> GetHistoryAsync(Guid userId)
         {
             var user = await _userRepository.GetAsync(userId);
             if (user == null) throw new MyException(MyCodesNumbers.UserNotFound, MyCodes.UserNotFound);
@@ -88,20 +70,25 @@ namespace PixBlocks_Addition.Infrastructure.Services
             return _mapper.Map<VideoHistory, VideoHistoryDto>(result);
         }
 
-        public async Task<int> GetUserProgres(Guid userId, Guid courseId)
+        public async Task<int> GetUserProgressAsync(Guid userId, Guid courseId)
         {
             var user = await _userRepository.GetAsync(userId);
-            var videoHistory = await _videoHistoryRepository.GetAsync(user);
+            if (user == null)
+                throw new MyException(MyCodesNumbers.UserNotFound, MyCodes.UserNotFound);
+
             var course = await _courseRepository.GetAsync(courseId);
-            if (user == null) throw new MyException(MyCodesNumbers.UserNotFound, MyCodes.UserNotFound);
-            if (videoHistory == null) throw new MyException(MyCodesNumbers.VideoHistoryNotFound, MyCodes.VideoHistoryNotFound);
-            if (course == null) throw new MyException(MyCodesNumbers.CourseNotFound, MyCodes.CourseNotFound);
+            if (course == null)
+                throw new MyException(MyCodesNumbers.CourseNotFound, MyCodes.CourseNotFound);
+
+            var videoHistory = await _videoHistoryRepository.GetAsync(user);
+            if (videoHistory == null)
+                throw new MyException(MyCodesNumbers.VideoHistoryNotFound, MyCodes.VideoHistoryNotFound);
 
             long time = 0;
-            foreach(var i in videoHistory.Videos)
+            foreach(var record in videoHistory.Videos)
             {
-               if (i.Video.ParentId == courseId)
-                    time += i.Time;
+               if (record.Video.ParentId == courseId)
+                    time += record.Time;
             }
             int percentage = (int)(time / course.Length) * 100;
             return percentage;
