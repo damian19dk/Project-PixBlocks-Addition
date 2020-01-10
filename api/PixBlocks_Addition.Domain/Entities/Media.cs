bb85beb88288ac24baa9c0a5360d4196eb5e3047
Piotrek <p.popowski01@gmail.com>
2020-01-10 14:22:59 +0100

    Remove category

--- api/PixBlocks_Addition.Domain/Entities/Media.cs
+++ api/PixBlocks_Addition.Domain/Entities/Media.cs
@@ -15,7 +15,6 @@ namespace PixBlocks_Addition.Domain.Entities
         public Guid Id { get; protected set; }
         public int Index { get; protected set; }
         public string MediaId { get; protected set; }
-        public Category Category { get; protected set; }
         public Guid ParentId { get; protected set; }
         public bool Premium { get; protected set; }
         public string Title { get; protected set; }
