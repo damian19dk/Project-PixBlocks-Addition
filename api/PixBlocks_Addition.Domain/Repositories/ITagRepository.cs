6b11b605a7d97d57d458ca9f26c62254e68548d6
Piotrek <p.popowski01@gmail.com>
2020-01-10 15:46:20 +0100

    Add Tag repository

---
+++ api/PixBlocks_Addition.Domain/Repositories/ITagRepository.cs
@@ -0,0 +1,19 @@
+﻿using PixBlocks_Addition.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PixBlocks_Addition.Domain.Repositories
+{
+    public interface ITagRepository
+    {
+        Task AddAsync(Tag tag);
+        Task<Tag> GetAsync(Guid id);
+        Task<Tag> GetAsync(string name);
+        Task<IEnumerable<Tag>> BrowseAsync(string name);
+        Task<IEnumerable<Tag>> GetAllAsync();
+        Task UpdateAsync(Tag tag);
+        Task UpdateAsync(Tag[] tag);
+        Task RemoveAsync(Tag tag);
+    }
+}
