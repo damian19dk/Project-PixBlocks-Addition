100761c8fbdc656bf2eaa58ebb34f8d874218518
Piotrek <p.popowski01@gmail.com>
2020-01-10 15:48:19 +0100

    Add Tag service

---
+++ api/PixBlocks_Addition.Infrastructure/Services/ITagService.cs
@@ -0,0 +1,19 @@
+﻿using PixBlocks_Addition.Infrastructure.DTOs;
+using PixBlocks_Addition.Infrastructure.ResourceModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixBlocks_Addition.Infrastructure.Services
+{
+    public interface ITagService
+    {
+        Task CreateAsync(TagResource tag);
+        Task<TagDto> GetAsync(string name);
+        Task<IEnumerable<TagDto>> GetAllAsync();
+        Task<IEnumerable<TagDto>> BrowseAsync(string name);
+        Task UpdateAsync(string name, TagResource tag);
+        Task RemoveAsync(string name);
+    }
+}
---
+++ api/PixBlocks_Addition.Infrastructure/Services/TagService.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using PixBlocks_Addition.Domain.Entities;
+using PixBlocks_Addition.Domain.Exceptions;
+using PixBlocks_Addition.Domain.Repositories;
+using PixBlocks_Addition.Infrastructure.DTOs;
+using PixBlocks_Addition.Infrastructure.Mappers;
+using PixBlocks_Addition.Infrastructure.ResourceModels;
+
+namespace PixBlocks_Addition.Infrastructure.Services
+{
+    public class TagService : ITagService
+    {
+        private readonly ITagRepository _tagRepository;
+        private readonly IMapper _mapper;
+
+        public TagService(ITagRepository tagRepository, IAutoMapperConfig mapperConfig)
+        {
+            _tagRepository = tagRepository;
+            _mapper = mapperConfig.Mapper;
+        }
+
+        public async Task<IEnumerable<TagDto>> BrowseAsync(string name)
+        {
+            var tags = await _tagRepository.BrowseAsync(name);
+            return _mapper.Map<IEnumerable<TagDto>>(tags);
+        }
+
+        public async Task CreateAsync(TagResource tag)
+        {
+            var tagExists = await _tagRepository.GetAsync(tag.Name);
+            if (tagExists != null)
+            {
+                throw new MyException(MyCodesNumbers.TagExists, $"The tag {tag.Name} already exists.");
+            }
+
+            await _tagRepository.AddAsync(new Tag(tag.Name, tag.Description, tag.Color));
+        }
+
+        public async Task<IEnumerable<TagDto>> GetAllAsync()
+        {
+            var tags = await _tagRepository.GetAllAsync();
+            return _mapper.Map<IEnumerable<TagDto>>(tags);
+        }
+
+        public async Task<TagDto> GetAsync(string name)
+        {
+            var tag = await _tagRepository.GetAsync(name);
+            return _mapper.Map<TagDto>(tag);
+        }
+
+        public async Task RemoveAsync(string name)
+        {
+            var tag = await _tagRepository.GetAsync(name);
+            if(tag == null)
+            {
+                throw new MyException(MyCodesNumbers.TagNotFound, "The tag does not exist.");
+            }
+
+            await _tagRepository.RemoveAsync(tag);
+        }
+
+        public async Task UpdateAsync(string name, TagResource tag)
+        {
+            var tagEntity = await _tagRepository.GetAsync(name);
+            if(tagEntity == null)
+            {
+                throw new MyException(MyCodesNumbers.TagNotFound, "The tag does not exist.");
+            }
+
+            tagEntity.SetName(tag.Name);
+            tagEntity.SetDescription(tag.Description);
+            tagEntity.SetColor(tag.Color);
+
+            await _tagRepository.UpdateAsync(tagEntity);
+        }
+    }
+}
